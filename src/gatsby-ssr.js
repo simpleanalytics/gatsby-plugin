ab7f6c309e9ef5d53a3bf5240bff81114105c334
Adriaan <adriaanvanrossum@users.noreply.github.com>
2020-01-19 15:40:11 +0100

    Remove comments and rename both scripts to latest.js

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -7,10 +7,8 @@ exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
   let scriptName
   if (pluginOptions.scriptName) {
     scriptName = pluginOptions.scriptName
-  } else if (domain === defaultDomain) {
-    scriptName = 'latest.js'
   } else {
-    scriptName = 'app.js'
+    scriptName = 'latest.js'
   }
   const scriptUrl = `https://${domain}/${scriptName}`
 
@@ -21,7 +19,6 @@ exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
   }
 
   if (pluginOptions.metomic) {
-    // options.type = 'text/x-metomic'
     options.metomic = pluginOptions.metomic
   }
   if (pluginOptions.trackEvents) {
@@ -32,7 +29,6 @@ exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
     React.createElement('script', {
       id: 'simple-analytics-loader',
       key: 'plugin-simpleanalytics',
-      // type: 'text/javascript',
       type: options.metomic ? 'text/x-metomic' : 'text/javascript',
       ['data-micropolicy']: options.metomic,
       ['data-sa-global']: options.eventsGlobal,
